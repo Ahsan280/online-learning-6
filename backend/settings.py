202107a76b8b7a72eb2c3d7fbeb6d46dedf48263
Ahsan280 <ahsanrehman281@gmail.com>
2024-05-14 19:08:31 +0500

    aloo4

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d$-obma4-i)*&ngbzbhp2o77l%yj$*bzh(gpnul=4k=f%afwo&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
